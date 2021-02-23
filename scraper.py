fb310988d74c3f9eebd4ff6d02531c0cd52b4bf4
muzucode <parrottttt@users.noreply.github.com>
2021-02-23 00:54:14 -0500

    More updates.  Able to write 100s of box scores to CSVs.  Error eventually with HTML tag.

--- scraper.py
+++ scraper.py
@@ -1,16 +1,25 @@
 import requests
 import re
 import pandas as pd
+from datetime import datetime, timedelta
 import lxml
-pd.set_option('display.max_columns', 15)
+pd.set_option('display.max_columns', 4)
+pd.set_option('display.max_row', 2)
 pd.set_option('display.width', 1000)
 
-def getLeague():
 
-    URL = 'https://www.baseball-reference.com/leagues/MLB/2018-schedule.shtml'
+def getLeague(amtRequested, season):
+    # Initial HTML request
+    URL = 'https://www.baseball-reference.com/leagues/MLB/%s-schedule.shtml' % season
     page = requests.get(URL)
     response = page.text
     extensions = []
+    gamedates = []
+    # Game codes designate if game is in a double header or not
+    gamecodes = []
+    gameid = []
+    fullurls = []
+
 
     # Create substring of the boxes element
     ss = '&nbsp;&nbsp;&nbsp;&nbsp;<em><a href="/boxes'
@@ -19,7 +28,7 @@ def getLeague():
 
     numboxes = 0
 
-    # Creates list of box score URL extensions
+    # Creates list of box score URL extensions and corresponding guide cols
     for i in range(len(matches_positions)):
         # Iterates until numboxes value breaks loop
         # Each loop adds one boxscore extension to be fetched
@@ -27,23 +36,36 @@ def getLeague():
         ind = matches_positions[i]
         line= response[ind:ind+66]
         extensions.append(line[38:])
-        print('Box score: %s' % i, extensions[i])
+        gamedates.append(extensions[i][-15:-7])
+        gamecodes.append(extensions[i][-7:-6])
+        id_string = extensions[i][-15:-7] + str(numboxes)
+        gameid.append(id_string)
+        fullurls.append('https://www.baseball-reference.com/' + extensions[i])
+        # Guidance Columns
+        print('Box score: %s' % i, extensions[i], ' || Date: ', gamedates[i], ' || Game code: ', gamecodes[i], ' || Game ID: ', gameid[i], ' || ', fullurls[i]  )
+        # If amtRequested is 999, loop will mine every box score from the season
+        if amtRequested != 999:
+            if numboxes == amtRequested:
+                break
 
-        # numTeams determines num of box score requests
-        if numboxes == 10:
-            break
-
-    return extensions
+    # Cols to be appended on to the final DFs for guidance/indexing purposes when analyzing
+    guidecols = [gamedates, gamecodes, gameid, fullurls]
+    return [extensions, guidecols]
 
 
-def getBoxes():
+def getBoxes(amtRequested, season):
     # Get all URL path extensions for boxscores
-    extensions = getLeague()
+    extensions, guidecols = getLeague(amtRequested, season)
+    # Get all corresponding guidance columns for each extension
     masterAwayBatting = []
     masterAwayPitching = []
     masterHomeBatting = []
     masterHomePitching = []
     masterBoxes = []
+
+    # If user inputs 999 games, change to # of box scores found
+    if amtRequested == 999:
+        amtRequested = len(extensions)
 
     # Cycle through all the extensions passed over
     for i in range(len(extensions)):
@@ -108,13 +130,36 @@ def getBoxes():
 
         # Assign teams with getTeams()
         teams = getTeams()
-        print('Preparing box scores for: ', teams)
+        print('Preparing box score # %s for: ' % i, teams)
 
-        # Save tables for away/home batting/pitching for individual box score
-        awaybatting = pd.read_html(source, attrs = {'id': '%sbatting' % teams[0]})
-        homebatting = pd.read_html(source, attrs = {'id': '%sbatting' % teams[1]})
-        awaypitching = pd.read_html(source, attrs={'id': '%spitching' % teams[0]})
-        homepitching = pd.read_html(source, attrs={'id': '%spitching' % teams[1]})
+        # Save dataframes for away/home batting/pitching for individual box score
+        awaybatting = pd.read_html(source, attrs = {'id': '%sbatting' % teams[0]}, header= 0)
+        homebatting = pd.read_html(source, attrs = {'id': '%sbatting' % teams[1]}, header= 0)
+        awaypitching = pd.read_html(source, attrs={'id': '%spitching' % teams[0]}, header= 0)
+        homepitching = pd.read_html(source, attrs={'id': '%spitching' % teams[1]}, header= 0)
+
+        # Add Guidance Columns to each dataframe
+        # Each of these are a LIST OF DATAFRAMES, MUST BE INDEXED!
+        counter = 0
+        for a_list in [awaybatting, homebatting, awaypitching, homepitching]:
+
+            if counter % 2 == 0:
+                a_list[0]['HomeAway'] = 'Away'
+            else:
+                a_list[0]['HomeAway'] = 'Home'
+
+            if counter == 0 or counter == 1:
+                a_list[0]['BattingPitching'] = 'Batting'
+            else:
+                a_list[0]['BattingPitching'] = 'Pitching'
+
+
+            a_list[0]['Date'] = guidecols[0][i]
+            a_list[0]['Code'] = guidecols[1][i]
+            a_list[0]['ID'] = guidecols[2][i]
+            a_list[0]['Full URL'] = guidecols[3][i]
+
+            counter+=1
 
         # Append individual box scores to masters
         masterAwayBatting.append(awaybatting)
@@ -122,15 +167,42 @@ def getBoxes():
         masterAwayPitching.append(awaypitching)
         masterHomePitching.append(homepitching)
 
-    masterBoxes.append(masterAwayBatting)
-    masterBoxes.append(masterHomeBatting)
-    masterBoxes.append(masterAwayPitching)
-    masterBoxes.append(masterHomePitching)
+    finalAwayBatting  = pd.DataFrame()
+    finalHomeBatting  = pd.DataFrame()
+    finalAwayPitching = pd.DataFrame()
+    finalHomePitching = pd.DataFrame()
+    for v in range(amtRequested):
+        finalAwayBatting = pd.concat([finalAwayBatting,masterAwayBatting[v][0]])
+        finalHomeBatting = pd.concat([finalHomeBatting,masterHomeBatting[v][0]])
+        finalAwayPitching = pd.concat([finalAwayPitching,masterAwayPitching[v][0]])
+        finalHomePitching = pd.concat([finalHomePitching,masterHomePitching[v][0]])
+
+
+    # Remove all the 'Team Totals' rows since I don't need them
+    finalAwayBatting = finalAwayBatting[finalAwayBatting['Batting'] != 'Team Totals']
+    finalHomeBatting = finalHomeBatting[finalHomeBatting['Batting'] != 'Team Totals']
+    finalAwayPitching = finalAwayPitching[finalAwayPitching['Pitching'] != 'Team Totals']
+    finalHomePitching = finalHomePitching[finalHomePitching['Pitching'] != 'Team Totals']
+
+    # Group all the final DFs together for return
+    masterBoxes = [
+        finalAwayBatting,
+        finalHomeBatting,
+        finalAwayPitching,
+        finalHomePitching
+    ]
+
+    # Write to CSVs
+    finalAwayBatting.to_csv('AwayBatting%s.csv' % season, sep=',')
+    finalHomeBatting.to_csv('HomeBatting%s.csv' % season, sep=',')
+    finalAwayPitching.to_csv('AwayPitching%s.csv' % season, sep=',')
+    finalHomePitching.to_csv('HomePitching%s.csv' % season, sep=',')
 
     return masterBoxes
 
-        # print(teams)
 
 
-print(getBoxes())
+
+# First parameter of 999 will give you entire season
+print(getBoxes(300,'2015'))
 print('Above is a list of all away/home batting/pitching data for the specified number of games')
